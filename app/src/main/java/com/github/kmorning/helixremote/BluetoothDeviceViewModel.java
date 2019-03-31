6bccebdc67058bf199ff7799b65d948252ae9f43
kmorning <kleemorning@gmail.com>
2019-03-31 15:17:23 -0400

    Add empty fragment and viewmodel to handle bluetooth device selection.

---
+++ app/src/main/java/com/github/kmorning/helixremote/BluetoothDeviceViewModel.java
@@ -0,0 +1,7 @@
+package com.github.kmorning.helixremote;
+
+import android.arch.lifecycle.ViewModel;
+
+public class BluetoothDeviceViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
